1ba0ca3ea385d232c782e8ec444424717df80552
WagnerJM <45420760+WagnerJM@users.noreply.github.com>
2019-06-28 14:11:53 +0200

    added SignUp Form

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "../views/Home.vue";
 
 import Login from "../views/auth/Login.vue";
+import SignUp from "../views/auth/SignUp.vue";
 
 import store from "../store";
 Vue.use(Router);
@@ -11,10 +12,16 @@ export default new Router({
   mode: "history",
   routes: [
     {
+      path: "/signup",
+      name: "signup",
+      component: SignUp
+    },
+    {
       path: "/login",
       name: "login",
       component: Login
     },
+
     {
       path: "/",
       name: "home",
---
+++ src/views/auth/SignUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <form>
+      <h2>Registrieren</h2>
+      <label>Username</label>
+      <input type="text" name="username" placeholder="Username...">
+      <label>E-mail</label>
+      <input type="text" name="email" placeholder="Email...">
+      <label>Password</label>
+      <input type="password" name="password" placeholder="Password...">
+      <button>Registrieren</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
